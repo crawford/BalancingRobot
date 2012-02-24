7f1b7254f6e6f16ce25c2ffa1baa6a9ab43bd73e
Alex Crawford <adrofwarc@gmail.com>
2012-02-24 15:50:37 -0500

    Fixed accelerometer

--- src/accelerometer.c
+++ src/accelerometer.c
@@ -24,15 +24,14 @@
 #define AIG_ADDRESS      (BASE_ADDRESS + 3)	// Analog Input Gain Register
 #define INT_ADDRESS      (BASE_ADDRESS + 4)	// Interrupt Control Register
 #define FIFO_THR_ADDRESS (BASE_ADDRESS + 5)	// FIFO Threshold Control Register
+#define FIFO_DEP_ADDRESS (BASE_ADDRESS + 6)	// FIFO Depth Register
 #define STATUS_ADDRESS   (BASE_ADDRESS + 7)	// Status Register
 
-#define ANALOG_GAIN		    0x02    // 0V - 5V
+#define ANALOG_GAIN		    0x06    // 0V - 5V
 #define ANALOG_HIGH_CHANNEL	0x02
 #define ANALOG_LOW_CHANNEL	0x01
 #define ANALOG_CHANNEL_RANGE ((ANALOG_HIGH_CHANNEL << 4) | ANALOG_LOW_CHANNEL)
 
-#define X_MIN 9400
-#define X_MAX 31400
 #define Y_MIN 12300
 #define Y_MAX 32300
 #define Z_MIN 8300
@@ -49,27 +48,45 @@ static uintptr_t aigHandle;
 static uintptr_t intHandle;
 static uintptr_t fthHandle;
 static uintptr_t statusHandle;
+static uintptr_t fifoDepthHandle;
 
 
-int16_t axis_read();
 
 /**
  * Thread to read values from the accelerometer.
  */
 void accel_thread(union sigval s) {
-	int channel = in8(statusHandle) & 0x0F;
-	float Y;
-	float Z;
-	if (channel == 1) {
-		Y = (axis_read() - (Y_MIN + Y_MAX)/2) * PI / (Y_MAX - Y_MIN);
-		Z = (axis_read() - (Z_MIN + Z_MAX)/2) * PI / (Z_MAX - Z_MIN);
-	} else {
-		Z = (axis_read() - (Z_MIN + Z_MAX)/2) * PI / (Z_MAX - Z_MIN);
-		Y = (axis_read() - (Y_MIN + Y_MAX)/2) * PI / (Y_MAX - Y_MIN);
+	int waitReading;
+	int converted = 0;
+
+	uint16_t value = 0;
+
+	for (waitReading = 0; waitReading < 10000; waitReading++) {
+		if ((in8(aigHandle) & 0x80) == 0) {
+			converted = 1;
+			break;
+		}
 	}
+
+	if (converted) {
+		value = in8(lsbHandle);
+		value |= in8(msbHandle) << 8;
+
+		float Y = (value - (Y_MIN + Y_MAX)/2) * PI / (Y_MAX - Y_MIN);
+
+
+		value = in8(lsbHandle);
+		value |= in8(msbHandle) << 8;
+
+		float Z = (value - (Z_MIN + Z_MAX)/2) * PI / (Z_MAX - Z_MIN);
+
 #ifdef DEBUG
-	printf("Y: %0.2f  Z: %0.2f    Angle: %0.2f\n", Y/PI, Z/PI, atan2f(Z, Y)/PI*180);
+		printf("Y: %0.2f  Z: %0.2f    Angle: %0.2f  FIFO: %d\n", Y/PI, Z/PI, atan2f(Z, Y)/PI*180, in8(fifoDepthHandle));
 #endif
+	}
+
+	//Start the next conversion
+	out8(baseHandle, 0x80);
 }
 
 void init_accelerometer() {
@@ -81,9 +98,11 @@ void init_accelerometer() {
 	intHandle = mmap_device_io(PORT_LENGTH, INT_ADDRESS);
 	fthHandle = mmap_device_io(PORT_LENGTH, FIFO_THR_ADDRESS);
 	statusHandle = mmap_device_io(PORT_LENGTH, STATUS_ADDRESS);
+	fifoDepthHandle = mmap_device_io(PORT_LENGTH, FIFO_DEP_ADDRESS);
 
-	// Set the FIFO threshold to 3
-	out8(fthHandle, 3);
+	// Set the FIFO threshold to 20
+	out8(fthHandle, 20);
+	out8(baseHandle, 0x10);
 
 	// Initialize ATD Channel
 	out8(atdHandle, ANALOG_CHANNEL_RANGE);
@@ -94,10 +113,15 @@ void init_accelerometer() {
 	// Allow configurations to settle
 	usleep(10);
 
+	out8(intHandle, in8(intHandle) & ~0x01);
+
 	// Enable analog interrupts
 	/*out8(intHandle, in8(intHandle) | 0x09);
 	out8(intHandle, in8(intHandle) & ~0x10);
 	register_isr(&accelerometer_isr, ADC_ISR);*/
+
+	// Initiate the first conversion
+	out8(baseHandle, 0x80);
 
 	// Setup the ATD timer.
 	struct sigevent event;
@@ -109,32 +133,4 @@ void init_accelerometer() {
 	struct timespec millisec = { .tv_sec = 0, .tv_nsec = 100000000 };
 	struct itimerspec time_run = { .it_value = millisec, .it_interval = millisec };
 	timer_settime(timer, 0, &time_run, NULL);
-}
-
-/**
- * Read a value from the ATD.
- * @return -32768 to 32767
- */
-int16_t axis_read() {
-	int waitReading;
-	int converted = 0;
-
-	uint8_t lsbData = 0;
-	uint8_t msbData = 0;
-
-	out8(baseHandle, 0x80);
-
-	for (waitReading = 0; waitReading < 10000; waitReading++) {
-		if ((in8(aigHandle) & 0x80) == 0) {
-			converted = 1;
-			break;
-		}
-	}
-
-	if (converted) {
-		lsbData = in8(lsbHandle);
-		msbData = in8(msbHandle);
-	}
-
-	return (msbData << 8) | lsbData;
 }
